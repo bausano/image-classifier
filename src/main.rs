8a87b584dbf012be5fe4d426717ac67005c03f4c
Michael Bausano <michael@joinpouch.com>
2019-02-10 17:21:29 +0000

    Commenting main

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ extern crate rand;
 pub mod reader;
 pub mod neural_network;
 
-use std::time::{SystemTime};
 use neural_network::network::Network;
+use std::time::{SystemTime, Duration};
 use neural_network::activation::Activation;
 
 fn main() {
@@ -15,9 +15,52 @@ fn main() {
     12_f64,
   );
 
-  // Trains the network on the training data.
-  train_network(&mut network, 1000);
+  let iterations = 1000;
 
+  // Trains the network on the training data.
+  let (duration, samples) = train_network(&mut network, 1000);
+
+  // We read the digits from input file.
+  let (success, total) = validate_network(&network);
+
+  println!(
+    "Done {} iterations on {} samples in {:?}.", iterations, samples, duration,
+  );
+
+  println!("Correct {} out of {}.", success, total);
+}
+
+/// Trains the network on the input testing data.
+/// TODO: Dynamicaly load the input file.
+///
+/// @param network Network instance to train
+/// @param iterations How many times should be the data set processed
+/// @return Tuple in format (duration_of_training, samples_in_per_data_set)
+fn train_network (network: &mut Network, iterations: usize) -> (Duration, usize) {
+  // We read the digits from input file.
+  let training_data: Vec<(u8, Vec<f64>)> = reader::read_digits(
+    include_str!("../data/input.txt"),
+  ) .iter()
+    .map(|digit| (digit.class, digit.grid.clone()))
+    .collect();
+  let started_at = SystemTime::now();
+
+  // Training the network.
+  for _ in 0..iterations {
+    network.train(&training_data);
+  }
+
+  (
+    SystemTime::now().duration_since(started_at).unwrap(),
+    training_data.len(),
+  )
+}
+
+/// Performs a cross fold validation on the training set.
+///
+/// @param network Trained network
+/// @return Tuple in format (successful_classifications, samples)
+fn validate_network (network: &Network) -> (usize, usize) {
   // We read the digits from input file.
   let testing_data: Vec<(u8, Vec<f64>)> = reader::read_digits(
     include_str!("../data/cross_fold.txt"),
@@ -32,31 +75,7 @@ fn main() {
       if succeded { success + 1 } else { success }
     });
 
-  println!("Correct {} out of {}.", success, testing_data.len());
-
-  // TODO: Extract one input from the file and try to classify it.
-}
-
-fn train_network (network: &mut Network, iterations: usize) {
-  // We read the digits from input file.
-  let training_data: Vec<(u8, Vec<f64>)> = reader::read_digits(
-    include_str!("../data/input.txt"),
-  ) .iter()
-    .map(|digit| (digit.class, digit.grid.clone()))
-    .collect();
-  let started_at = SystemTime::now();
-
-  // Training the network.
-  for _ in 0..iterations {
-    network.train(&training_data);
-  }
-
-  println!(
-    "Done {} iterations on {} samples in {:?}.",
-    iterations,
-    training_data.len(),
-    SystemTime::now().duration_since(started_at).unwrap(),
-  );
+  (success, testing_data.len())
 }
 
 #[cfg(test)]
